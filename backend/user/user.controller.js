aa8b2c2986053e8919e4cf752a88d182b0d060f1
Daniel Firsht <dfirsht@us.ibm.com>
2016-11-23 14:03:36 -0600

    further worked on auth flow

--- backend/user/user.controller.js
+++ backend/user/user.controller.js
@@ -4,4 +4,39 @@
 
 var userController = {};
 
+var userDB = [];
+
+userController.registerUser = function(user) {
+    return userController.saveUser(user);
+};
+
+userController.saveUser = function (user) {
+    return new Promise(function (resolve, reject) {
+        var position = userDB.map(function (e) {
+            return e.id
+        }).indexOf(user.id);
+        if(position != -1) {
+            userDB[position] = user
+        }
+        else {
+            userDB.push(user);
+        }
+        resolve();
+    });
+};
+
+userController.getUserByID = function (id) {
+  return new Promise(function (resolve, reject) {
+      var position = userDB.map(function (e) {
+          return e.id
+      }).indexOf(id);
+      if(position != -1) {
+          resolve(userDB[position]);
+      }
+      else {
+          reject("user not found");
+      }
+  })
+};
+
 module.exports = userController;
