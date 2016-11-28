65a56d69537ed149f9a2a2c8617c3a7ba3a95732
Daniel Firsht <dfirsht@us.ibm.com>
2016-11-28 13:33:16 -0600

    Auth through jwt to get custom account info

--- backend/auth/index.js
+++ backend/auth/index.js
@@ -13,16 +13,14 @@ var router = express.Router();
 
 router.use('/facebook', require('./facebook'));
 
-router.get('/success', function (req, res, next) {
-    res.send(req.user).end();
-});
+router.get('/success', authService.handleLoginSuccess);
 
 router.get('/failure', function (req, res, next) {
     res.send("failed to authenticate");
 });
 
-// router.get('/me', authService.isAuthenticated(), function (req, res, next) {
-//    res.json(req.user);
-// });
+router.get('/me', authService.isAuthenticated(), function (req, res, next) {
+   res.json(req.user);
+});
 
 module.exports = router;
