5f1552dca51a5e807d0715bc0058ff67ce489278
Nathan Hekman <hekman@us.ibm.com>
2016-11-29 16:38:43 -0600

    added YTPlayerView and sample playlist

--- iOS/Stormtrooper/Stormtrooper/Controllers/MainViewController.swift
+++ iOS/Stormtrooper/Stormtrooper/Controllers/MainViewController.swift
@@ -9,11 +9,34 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var playerView: YTPlayerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setupPlayerView()
+        
     }
+    
+    private func setupPlayerView() {
+        self.playerView.delegate = self
+        self.playerView.load(withVideoId: "4NFDhxhWyIw", playerVars: [
+            "playsinline" : 1,
+            "modestbranding" : 0,
+            "playlist": "RTDuUiVSCo4"
+            ])
+        
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        self.playerView.playVideo()
+    }
+    
+    @IBAction func pauseTapped(_ sender: Any) {
+        self.playerView.pauseVideo()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -23,3 +46,13 @@ class MainViewController: UIViewController {
 
 }
 
+extension MainViewController: YTPlayerViewDelegate {
+    func playerView(_ playerView: YTPlayerView, didPlayTime playTime: Float) {
+
+    }
+    
+    func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
+
+    }
+    
+}
