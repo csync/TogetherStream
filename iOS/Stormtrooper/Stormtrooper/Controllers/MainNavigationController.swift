9dd74181b3526135957e8b67ea5e75151e3cb7cb
Nathan Hekman <hekman@us.ibm.com>
2016-12-07 12:01:19 -0600

    stubbed out entire create stream flow

---
+++ iOS/Stormtrooper/Stormtrooper/Controllers/MainNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  MainNavigationController.swift
+//  Stormtrooper
+//
+//  Created by Nathan Hekman on 12/7/16.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
