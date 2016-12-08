a650a9d1cfafa7ae7110867ffc80f321a981dd63
Nathan Hekman <hekman@us.ibm.com>
2016-12-07 18:50:44 -0600

    added text and email invites

--- iOS/Stormtrooper/Stormtrooper/Controllers/InviteStreamViewController.swift
+++ iOS/Stormtrooper/Stormtrooper/Controllers/InviteStreamViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class InviteStreamViewController: UIViewController {
 
@@ -39,5 +40,73 @@ class InviteStreamViewController: UIViewController {
         streamVC.navigationItem.hidesBackButton = true
         self.navigationController?.pushViewController(streamVC, animated: true)
     }
+    
+    @IBAction func textTapped(_ sender: Any) {
+        let messageVC = MFMessageComposeViewController()
+        
+        messageVC.body = "Download Stormtrooper to join my Stream: http://ibm.biz/BdsMEz";
+        //messageVC.recipients = [""]
+        messageVC.messageComposeDelegate = self
+        
+        self.present(messageVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func emailTapped(_ sender: Any) {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        //mailComposerVC.setToRecipients([""])
+        mailComposerVC.setSubject("Check this out!")
+        mailComposerVC.setMessageBody("Download Stormtrooper to join my Stream: http://ibm.biz/BdsMEz", isHTML: false)
+        
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposerVC, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertController(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", preferredStyle: .alert)
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+        
+    }
 
+}
+
+extension InviteStreamViewController: MFMessageComposeViewControllerDelegate {
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch (result) {
+        case .cancelled:
+            print("Message was cancelled")
+            controller.dismiss(animated: true, completion: nil)
+        case .failed:
+            print("Message failed")
+            controller.dismiss(animated: true, completion: nil)
+        case .sent:
+            print("Message was sent")
+            controller.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+}
+
+extension InviteStreamViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch (result) {
+        case .cancelled:
+            print("Message was cancelled")
+            controller.dismiss(animated: true, completion: nil)
+        case .failed:
+            print("Message failed")
+            controller.dismiss(animated: true, completion: nil)
+        case .sent:
+            print("Message was sent")
+            controller.dismiss(animated: true, completion: nil)
+        case .saved:
+            print("Message was saved")
+            controller.dismiss(animated: true, completion: nil)
+        }
+    }
 }
