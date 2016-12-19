6e4a037ecb1a21ec1a58bfdcbb2143267df1ec7d
Daniel Firsht <dfirsht@us.ibm.com>
2016-12-19 14:17:46 -0600

    added method to fetch profile image

--- iOS/Stormtrooper/Stormtrooper/Models/User.swift
+++ iOS/Stormtrooper/Stormtrooper/Models/User.swift
@@ -6,18 +6,22 @@
 //  Copyright © 2016 IBM. All rights reserved.
 //
 
+import Foundation
+import UIKit
+
 struct User {
 	let id: String
 	let name: String
 	let pictureURL: String
+	var profileImage: UIImage?
 	
 	/*
 	Response format of GraphAPI 2.8
 	"name": "<name>",
 	"picture": {
 		"data": {
-		"is_silhouette": <bool>,
-		"url": "<url>"
+			"is_silhouette": <bool>,
+			"url": "<url>"
 		}
 	},
 	"id": "<id>"
@@ -27,5 +31,21 @@ struct User {
 		id =  facebookResponse["id"] as? String ?? ""
 		name =  facebookResponse["name"] as? String ?? ""
 		pictureURL = picture?["url"] as? String ?? ""
+	}
+	
+	/// Note: Does not set profileImage var
+	func fetchProfileImage(callback: @escaping (UIImage?) -> Void) {
+		guard let url = URL(string: pictureURL) else {
+			callback(nil)
+			return
+		}
+		URLSession.shared.dataTask(with: url) {data, response, error in
+			if let data = data {
+				callback(UIImage(data: data))
+			}
+			else {
+				
+			}
+		}
 	}
 }
