6400280d4b2d80b0664b5db76ff0fe0d023eaba2
Daniel Firsht <dfirsht@us.ibm.com>
2017-01-04 14:27:01 -0600

    added method to stream to listen for current video

--- iOS/Stormtrooper/Stormtrooper/Models/Stream.swift
+++ iOS/Stormtrooper/Stormtrooper/Models/Stream.swift
@@ -6,10 +6,14 @@
 //  Copyright © 2017 IBM. All rights reserved.
 //
 
+import CSyncSDK
+
 struct Stream {
 	let name: String
 	let hostID: String
 	let csyncPath: String
+	
+	private var key: Key
 	
 	init?(jsonDictionary: [String: String]) {
 		guard let hostID = jsonDictionary["user_id"], let csyncPath = jsonDictionary["csync_path"], let name = jsonDictionary["stream_name"] else {
@@ -18,5 +22,23 @@ struct Stream {
 		self.name = name
 		self.hostID = hostID
 		self.csyncPath = csyncPath
+		key = CSyncDataManager.sharedInstance.createKey(atPath: csyncPath + ".currentVideoID")
 	}
+	
+	func listenForCurrentVideo(callback: @escaping (Error?, String?) -> Void) {
+		key.listen {value, error in
+			if let value = value?.data {
+				callback(nil, value)
+			}
+			else {
+				callback(error, nil)
+			}
+		}
+	}
+	
+	func stopListeningForCurrentVideo() {
+		key.unlisten()
+	}
+	
+	
 }
