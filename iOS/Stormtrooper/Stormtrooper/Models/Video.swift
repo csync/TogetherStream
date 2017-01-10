c35171a6b33e9e85e51b66cf5365d8c62e26d009
Daniel Firsht <dfirsht@us.ibm.com>
2017-01-10 15:40:10 -0600

    added channel title to video model

--- iOS/Stormtrooper/Stormtrooper/Models/Video.swift
+++ iOS/Stormtrooper/Stormtrooper/Models/Video.swift
@@ -10,26 +10,29 @@ struct Video {
 	let id: String
 	let title: String
 	let thumbnailURL: String
+	let channelTitle: String
 	
 	init?(listVideoData: [String: Any]) {
 		let snippetInfo = listVideoData["snippet"] as? [String: Any]
-		guard let id = listVideoData["id"] as? String, let title = (snippetInfo?["localized"] as? [String: String])?["title"], let thumbnailURL = (snippetInfo?["thumbnails"] as? [String: [String : Any]])?["high"]?["url"] as? String else {
+		guard let id = listVideoData["id"] as? String, let title = (snippetInfo?["localized"] as? [String: String])?["title"], let thumbnailURL = (snippetInfo?["thumbnails"] as? [String: [String : Any]])?["high"]?["url"] as? String, let channelTitle = snippetInfo?["channelTitle"] as? String else {
 			return nil
 		}
 		
 		self.id = id
 		self.title = title
 		self.thumbnailURL = thumbnailURL
+		self.channelTitle = channelTitle
 	}
 	
 	init?(searchResultData: [String: Any]) {
 		let snippetInfo = searchResultData["snippet"] as? [String: Any]
-		guard let id = (searchResultData["id"] as? [String: String])?["videoId"], let title = snippetInfo?["title"] as? String, let thumbnailURL = (snippetInfo?["thumbnails"] as? [String: [String : Any]])?["high"]?["url"] as? String else {
+		guard let id = (searchResultData["id"] as? [String: String])?["videoId"], let title = snippetInfo?["title"] as? String, let thumbnailURL = (snippetInfo?["thumbnails"] as? [String: [String : Any]])?["high"]?["url"] as? String, let channelTitle = snippetInfo?["channelTitle"] as? String else {
 			return nil
 		}
 		
 		self.id = id
 		self.title = title
 		self.thumbnailURL = thumbnailURL
+		self.channelTitle = channelTitle
 	}
 }
