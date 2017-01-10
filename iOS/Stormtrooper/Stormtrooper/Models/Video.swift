fdeb13f4004022fc6adeacc7f0ac0d2a9f4c8a88
Daniel Firsht <dfirsht@us.ibm.com>
2017-01-10 16:04:18 -0600

    check that videos are embeddable

--- iOS/Stormtrooper/Stormtrooper/Models/Video.swift
+++ iOS/Stormtrooper/Stormtrooper/Models/Video.swift
@@ -13,6 +13,10 @@ struct Video {
 	let channelTitle: String
 	
 	init?(listVideoData: [String: Any]) {
+		// Check to make sure the video can be played in app
+		if (listVideoData["status"] as? [String: Any])?["embeddable"] as? Bool != true {
+			return nil
+		}
 		let snippetInfo = listVideoData["snippet"] as? [String: Any]
 		guard let id = listVideoData["id"] as? String, let title = (snippetInfo?["localized"] as? [String: String])?["title"], let thumbnailURL = (snippetInfo?["thumbnails"] as? [String: [String : Any]])?["high"]?["url"] as? String, let channelTitle = snippetInfo?["channelTitle"] as? String else {
 			return nil
