44fafc0a020a5f99dac89e22d696ea257b9c804a
Nathan Hekman <hekman@us.ibm.com>
2017-01-17 17:56:12 -0600

    more final design for keyboard view

--- iOS/Stormtrooper/Stormtrooper/Views/ChatTextFieldAccessoryView.swift
+++ iOS/Stormtrooper/Stormtrooper/Views/ChatTextFieldAccessoryView.swift
@@ -10,17 +10,14 @@ import UIKit
 
 class ChatTextFieldAccessoryView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    @IBOutlet weak var cancelButton: UIButton!
+
     
     class func instanceFromNib() -> ChatTextFieldAccessoryView {
         return UINib(nibName: "ChatTextFieldAccessoryView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! ChatTextFieldAccessoryView
     }
+    
+    
     
 
 }
