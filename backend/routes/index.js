400f94ac25ac4be94340c3cc6f1d3d07f7bbfe84
Jaime Guajardo <guajardo@us.ibm.com>
2017-01-19 16:43:43 -0600

    adding in the basic landing page copied from ICT’s repo. removed many of the assets and html files that aren’t the main page. can add them in if they’re needed once designs are in.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,14 @@ var invites = require('../invites');
 var id = require('../id');
 var router = express.Router();
 var authService = require('../auth/auth.service');
+var favicon = require('serve-favicon');
+
+// Constants
+var sixHours = 21600;
+var standardOptions = {
+  maxAge: sixHours,
+  root: __dirname
+};
 
 router.use("/auth", auth);
 
@@ -11,9 +19,24 @@ router.use("/invites", authService.isAuthenticated(), invites);
 
 router.use("/id", id);
 
+// Loading assets
+router.use(express.static('./public', {
+  maxAge: sixHours
+}));
+
+router.use(favicon('./public/favicon.ico'));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Hello Stormtrooper").end();
+  res.set({
+    "Cache-Control": "public, max-age=21600"
+  });
+  res.sendFile('index.html', standardOptions, function(err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).send(err);
+    }
+  });
 });
 
 module.exports = router;
