c0ac094f648265cf491b38160f3ef52e16e0fbac
Glenn R. Fisher <glennrfisher@gmail.com>
2017-01-23 14:40:21 -0600

    Add findBestViewController

--- iOS/Stormtrooper/Stormtrooper/Extensions/UIViewControllerExtension.swift
+++ iOS/Stormtrooper/Stormtrooper/Extensions/UIViewControllerExtension.swift
@@ -32,4 +32,43 @@ extension UIViewController {
         }
 
     }
+    
+    /**
+     Find the view controller that the user is most likely interacting with.
+ 
+     - parameter from: The view controller from which to begin the search.
+     - returns: The view controller that the user is most likely interacting with.
+     */
+    static func findBestViewController(from viewController: UIViewController) -> UIViewController {
+        if let presentedViewController = viewController.presentedViewController {
+            // recurse using the presented view controller
+            return findBestViewController(from: presentedViewController)
+        }
+        
+        if let viewController = viewController as? UISplitViewController {
+            if let primary = viewController.viewControllers.first {
+                // recurse using the primary view controller
+                return findBestViewController(from: primary)
+            }
+            return viewController
+        }
+        
+        if let viewController = viewController as? UINavigationController {
+            if let top = viewController.viewControllers.last {
+                // recuse using the top view controller
+                return findBestViewController(from: top)
+            }
+            return viewController
+        }
+        
+        if let viewController = viewController as? UITabBarController {
+            if let selected = viewController.selectedViewController {
+                // recurse using the selected view controller
+                return findBestViewController(from: selected)
+            }
+            return viewController
+        }
+        
+        return viewController
+    }
 }
