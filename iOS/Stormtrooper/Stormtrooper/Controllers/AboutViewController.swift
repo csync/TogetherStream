b8bc3fce4cc296fe3288ba0dfa3c84a3f39f2543
Glenn R. Fisher <glennrfisher@gmail.com>
2017-02-06 14:16:33 -0600

    Update profile screens (#238)
    * Add assets for about page
    
    * Update assets for about page
    
    * Convert images to buttons with background
    
    * Add AboutViewController with logo actions
    
    * Center align the description
    
    * Update logo links
    
    * Open privacy policy and licenses with Safari view controller
    
    * Remove web view controller
    
    * Extend separators to edge of screen
    
    * Add separator above first cell
    
    * Add a zero-height footer to hide trailing empty cells
    
    * Open links in Safari instead of SFSafariViewController

---
+++ iOS/Stormtrooper/Stormtrooper/Controllers/AboutViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AboutViewController.swift
+//  Stormtrooper
+//
+//  Created by Glenn R. Fisher on 2/2/17.
+//  Copyright © 2017 IBM. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    @IBAction func tappedCSync() {
+        open(url: "https://ibm.biz/together-stream-csync-logo")
+    }
+    
+    @IBAction func tappedBluemix() {
+        open(url: "https://ibm.biz/together-stream-bluemix-logo")
+    }
+    
+    @IBAction func tappedMIL() {
+        open(url: "https://ibm.biz/together-stream-mil-logo")
+    }
+    
+    @IBAction func tappedYoutube() {
+        open(url: "https://ibm.biz/together-stream-youtube-logo")
+    }
+    
+    private func open(url: String) {
+        guard let url = URL(string: url) else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+}
