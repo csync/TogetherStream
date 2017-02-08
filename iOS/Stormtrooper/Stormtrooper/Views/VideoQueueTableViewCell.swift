a9e3b209a7f9b96e03f7e302092fdbebe98b3596
Glenn R. Fisher <glennrfisher@gmail.com>
2017-02-07 19:04:33 -0600

    Update comments and formatting

--- iOS/Stormtrooper/Stormtrooper/Views/VideoQueueTableViewCell.swift
+++ iOS/Stormtrooper/Stormtrooper/Views/VideoQueueTableViewCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class VideoQueueTableViewCell: UITableViewCell {
+    
     @IBOutlet private weak var playImageView: UIImageView!
     @IBOutlet private weak var numberLabel: UILabel!
     @IBOutlet private weak var thumbnailImageView: UIImageView!
@@ -18,27 +19,35 @@ class VideoQueueTableViewCell: UITableViewCell {
     private let highlightedBackgroundColor = UIColor(red: 59/255, green: 59/255, blue: 59/255, alpha: 1.0)
     private let unhighlightedBackgroundColor = UIColor(red: 40/255, green: 36/255, blue: 36/255, alpha: 1.0)
     
+    private var _isCurrentVideo = false
+    
+    /// The video's position in the playlist.
     var number: String? {
         get { return numberLabel.text }
         set { numberLabel.text = newValue }
     }
     
+    /// The video's thumbnail.
     var thumbnail: UIImage? {
         get { return thumbnailImageView.image }
         set { thumbnailImageView.image = newValue }
     }
     
+    /// The video's title.
     var title: String? {
         get { return titleLabel.text }
         set { titleLabel.text = newValue }
     }
     
+    /// The video's channel.
     var channel: String? {
         get { return channelLabel.text }
         set { channelLabel.text = newValue }
     }
     
     private var _isCurrentVideo = false
+    /// Is this the current video in the queue?
+    /// (If so, then its design will be updated.)
     var isCurrentVideo: Bool {
         get { return _isCurrentVideo }
         set {
