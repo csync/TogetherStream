fbdab14e2203b75ca1bd9b2ef75b35f5b9fa14b6
Glenn R. Fisher <glennrfisher@gmail.com>
2017-02-09 16:24:38 -0600

    Add setupBackButton to AddVideosViewController

--- iOS/Stormtrooper/Stormtrooper/Controllers/AddVideosViewController.swift
+++ iOS/Stormtrooper/Stormtrooper/Controllers/AddVideosViewController.swift
@@ -33,6 +33,7 @@ class AddVideosViewController: UIViewController {
         setupSearchBar()
         setupTableView()
         setupNavigationBar()
+        setupBackButton()
         
         streamNameLabel.text = "\"\(stream?.name ?? "")\" Queue".localizedUppercase
         
@@ -98,7 +99,19 @@ class AddVideosViewController: UIViewController {
     
     private func setupNavigationBar() {
         navigationItem.titleView = UIImageView(image: #imageLiteral(resourceName: "youTube"))
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+    
+    private func setupBackButton() {
+        let backButton = UIButton(type: .custom)
+        backButton.setImage(#imageLiteral(resourceName: "back_stream"), for: .normal)
+        backButton.frame = CGRect(x: 0, y: 0, width: 17, height: 17)
+        backButton.addTarget(self, action: #selector(backTapped), for: .touchUpInside)
+        let backButtonItem = UIBarButtonItem(customView: backButton)
+        navigationItem.setLeftBarButtonItems([backButtonItem], animated: false)
+    }
+    
+    @objc private func backTapped() {
+        let _ = navigationController?.popViewController(animated: true)
     }
 	
     @IBAction func doneTapped(_ sender: Any) {
