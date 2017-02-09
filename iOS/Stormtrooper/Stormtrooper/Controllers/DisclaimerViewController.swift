426b56fe29751433e9affdd1e057245c77ff50a8
Glenn R. Fisher <glennrfisher@gmail.com>
2017-02-09 16:53:18 -0600

    Back button (#304)
    * Clean up MainNavigationController
    
    * Clean up HomeViewController
    
    * Clean up setupNavigationBar()
    
    * Clean up LoginViewController
    
    * Add helper function to setup profile button
    
    * Add setupBackButton to AboutViewController
    
    * Add setupBackButton to AddVideosViewController
    
    * Clean up AddVideosViewController
    
    * Clean up InviteStreamViewController
    
    * Clean up NameStreamViewController
    
    * Add setupBackButton to NameStreamViewController
    
    * Add setupBackButton to ProfileViewController
    
    * Cleanup InviteStreamViewController
    
    * Add setupBackButton to InviteStreamViewController
    
    * Add setupBackButton to DisclaimerViewController

--- iOS/Stormtrooper/Stormtrooper/Controllers/DisclaimerViewController.swift
+++ iOS/Stormtrooper/Stormtrooper/Controllers/DisclaimerViewController.swift
@@ -1,15 +1,14 @@
 //
-//  AboutViewController.swift
+//  DisclaimerViewController.swift
 //  Stormtrooper
 //
-//  Created by Glenn R. Fisher on 2/2/17.
+//  Created by Glenn R. Fisher on 2/9/17.
 //  Copyright © 2017 IBM. All rights reserved.
 //
 
 import UIKit
 
-class AboutViewController: UIViewController {
-    
+class DisclaimerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupBackButton()
@@ -24,30 +23,7 @@ class AboutViewController: UIViewController {
         navigationItem.setLeftBarButtonItems([backButtonItem], animated: false)
     }
     
-    private func open(url: String) {
-        guard let url = URL(string: url) else { return }
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-    
     @objc private func backTapped() {
         let _ = navigationController?.popViewController(animated: true)
     }
-    
-    @IBAction func tappedCSync() {
-        open(url: "https://ibm.biz/together-stream-csync-logo")
-    }
-    
-    @IBAction func tappedBluemix() {
-        open(url: "https://ibm.biz/together-stream-bluemix-logo")
-    }
-    
-    @IBAction func tappedMIL() {
-        open(url: "https://ibm.biz/together-stream-mil-logo")
-    }
-    
-    @IBAction func tappedYoutube() {
-        open(url: "https://ibm.biz/together-stream-youtube-logo")
-    }
-    
-    
 }
