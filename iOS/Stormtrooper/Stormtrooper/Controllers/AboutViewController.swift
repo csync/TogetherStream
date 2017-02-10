bd7df0cdb187a6fcf2c1bb172f25d38a0085eaca
Glenn R. Fisher <glennrfisher@gmail.com>
2017-02-10 13:05:38 -0600

    Clean up code in AboutViewController

--- iOS/Stormtrooper/Stormtrooper/Controllers/AboutViewController.swift
+++ iOS/Stormtrooper/Stormtrooper/Controllers/AboutViewController.swift
@@ -9,27 +9,13 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    @IBAction func tappedCSync()   { open(url: "https://ibm.biz/together-stream-csync-logo")   }
+    @IBAction func tappedBluemix() { open(url: "https://ibm.biz/together-stream-bluemix-logo") }
+    @IBAction func tappedMIL()     { open(url: "https://ibm.biz/together-stream-mil-logo")     }
+    @IBAction func tappedYoutube() { open(url: "https://ibm.biz/together-stream-youtube-logo") }
     
     private func open(url: String) {
         guard let url = URL(string: url) else { return }
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
-    
-    @IBAction func tappedCSync() {
-        open(url: "https://ibm.biz/together-stream-csync-logo")
-    }
-    
-    @IBAction func tappedBluemix() {
-        open(url: "https://ibm.biz/together-stream-bluemix-logo")
-    }
-    
-    @IBAction func tappedMIL() {
-        open(url: "https://ibm.biz/together-stream-mil-logo")
-    }
-    
-    @IBAction func tappedYoutube() {
-        open(url: "https://ibm.biz/together-stream-youtube-logo")
-    }
-    
-    
 }
