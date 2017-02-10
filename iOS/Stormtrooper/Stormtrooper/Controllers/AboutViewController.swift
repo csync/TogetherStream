b0e51e644241d5e3e190a3c5296f2c98a2b62ace
Glenn R. Fisher <glennrfisher@gmail.com>
2017-02-10 13:03:19 -0600

    Remove custom back button

--- iOS/Stormtrooper/Stormtrooper/Controllers/AboutViewController.swift
+++ iOS/Stormtrooper/Stormtrooper/Controllers/AboutViewController.swift
@@ -10,27 +10,9 @@ import UIKit
 
 class AboutViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupBackButton()
-    }
-    
-    private func setupBackButton() {
-        let backButton = UIButton(type: .custom)
-        backButton.setImage(#imageLiteral(resourceName: "back_stream"), for: .normal)
-        backButton.frame = CGRect(x: 0, y: 0, width: 17, height: 17)
-        backButton.addTarget(self, action: #selector(backTapped), for: .touchUpInside)
-        let backButtonItem = UIBarButtonItem(customView: backButton)
-        navigationItem.setLeftBarButtonItems([backButtonItem], animated: false)
-    }
-    
     private func open(url: String) {
         guard let url = URL(string: url) else { return }
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
-    
-    @objc private func backTapped() {
-        let _ = navigationController?.popViewController(animated: true)
     }
     
     @IBAction func tappedCSync() {
