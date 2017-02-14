5a3f01f23f45daf7c81051e59539d9d1988395b1
Jaime Guajardo <guajardo@us.ibm.com>
2017-02-14 16:28:02 -0600

    unignoring backend/config/private folder that contains credentials and stubbing them out.

---
+++ backend/config/private/credentials.js
@@ -0,0 +1,16 @@
+module.exports = {
+    facebook: {
+        appID: 'YOUR_FB_APP_ID',
+        secret: 'YOUR_FB_APP_SECRET'
+    },
+    google: {
+        clientID: 'YOUR_GOOGLE_CLIENT_ID',
+        clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+        apiKey: 'YOUR_GOOGLE_API_KEY'
+    },
+    app: {
+        accessTokenKey: Buffer.from('YOUR_ACCESS_TOKEN_KEY'),
+        refreshTokenKey: Buffer.from('YOUR_REFRESH_TOKEN_KEY')
+    },
+    sessionSecret: "YOUR_SESSION_SECRET"
+};
