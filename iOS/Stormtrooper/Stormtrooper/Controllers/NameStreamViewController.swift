4e662c7a715662805c47ace57d740969224c28ec
Daniel Firsht <dfirsht@gmail.com>
2017-02-15 12:29:48 -0600

    NameStreamVC marked up (#372)

--- iOS/Stormtrooper/Stormtrooper/Controllers/NameStreamViewController.swift
+++ iOS/Stormtrooper/Stormtrooper/Controllers/NameStreamViewController.swift
@@ -8,17 +8,22 @@
 
 import UIKit
 
+/// View controller for the "Naming Stream" screen
 class NameStreamViewController: UIViewController {
 
 	@IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
     
-    fileprivate var accessoryView: NextBannerView!
+    fileprivate var nextBannerView: NextBannerView!
     
+    /// Spacing frame to left pad the name text field.
     private let nameTextFieldSpacingFrame = CGRect(x: 0, y: 0, width: 18, height: 5)
+    /// Spacing insets to pad the description text view.
     private let descriptionTextViewSpacingInset = UIEdgeInsets(top: 14, left: 13, bottom: 10, right: 16)
     
+    /// The placeholder text of the description view.
     fileprivate let descriptionPlaceholder = "Description (optional)"
+    /// Determines if the text inside the description view is placeholder or not
     fileprivate var isDescriptionPlaceholder: Bool {
         return descriptionTextView.text == descriptionPlaceholder
     }
@@ -53,7 +58,9 @@ class NameStreamViewController: UIViewController {
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
     
+    /// Sets up the text fields
     private func setupTextFields() {
+        // Add padding
         nameTextField.leftViewMode = .always
         nameTextField.leftView = UIView(frame: nameTextFieldSpacingFrame)
         descriptionTextView.delegate = self
@@ -63,27 +70,33 @@ class NameStreamViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(nameTextFieldDidChange), name: NSNotification.Name.UITextFieldTextDidChange, object: nil)
     }
     
+    /// Sets up the add videos banner
     private func setupAddVideosBanner() {
         //new instance of accessory view
-        accessoryView = NextBannerView.instanceFromNib()
+        nextBannerView = NextBannerView.instanceFromNib()
         
         //add selector to dismiss and when editing to sync up both textfields
-        accessoryView.nextButton.addTarget(self, action: #selector(addVideosTapped), for: .touchUpInside)
+        nextBannerView.nextButton.addTarget(self, action: #selector(addVideosTapped), for: .touchUpInside)
         
         //actually set accessory view
-        nameTextField.inputAccessoryView = accessoryView
-        descriptionTextView.inputAccessoryView = accessoryView
+        nameTextField.inputAccessoryView = nextBannerView
+        descriptionTextView.inputAccessoryView = nextBannerView
     }
     
+    /// Shows or hides the next banner depending if the name text field
+    /// is empty.
     @objc private func nameTextFieldDidChange() {
         if nameTextField.text?.characters.count ?? 0 > 0 {
-            accessoryView.isHidden = false
+            nextBannerView.isHidden = false
         }
         else {
-            accessoryView.isHidden = true
+            nextBannerView.isHidden = true
         }
     }
     
+    /// When next banner is tapped, transition to "Add Videos" screen.
+    ///
+    /// - Parameter sender: The button tapped.
     @objc private func addVideosTapped(_ sender: Any) {
         Utils.sendGoogleAnalyticsEvent(withCategory: "Name", action: "SelectedAddVideos")
         Utils.sendGoogleAnalyticsEvent(withCategory: "Name", action: "DidAddDescription", value:
@@ -92,6 +105,7 @@ class NameStreamViewController: UIViewController {
         guard let addVideosVC = Utils.vcWithNameFromStoryboardWithName("addVideos", storyboardName: "AddVideos") as? AddVideosViewController else {
             return
         }
+        // Create stream object
         let facebookID = FacebookDataManager.sharedInstance.profile?.userID ?? ""
         let descriptionText = isDescriptionPlaceholder ? "" : descriptionTextView.text ?? ""
         let stream = Stream(
@@ -99,6 +113,8 @@ class NameStreamViewController: UIViewController {
             csyncPath: "streams.\(facebookID)",
             description: descriptionText,
             hostFacebookID: facebookID)
+        
+        // Configure AddVideosVC
 		addVideosVC.stream = stream
         addVideosVC.isCreatingStream = true
         self.navigationController?.pushViewController(addVideosVC, animated: true)
@@ -106,24 +122,21 @@ class NameStreamViewController: UIViewController {
 }
 
 extension NameStreamViewController: UITextFieldDelegate {
+    /// On text field return, change first responder to
+    /// description text field.
+    ///
+    /// - Parameter textField: The text field returning.
+    /// - Returns: If the text field should return.
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         descriptionTextView.becomeFirstResponder()
         return false
     }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        if textField.text?.characters.count ?? 0 > 0 {
-            accessoryView.isHidden = false
-        }
-        else {
-            accessoryView.isHidden = true
-        }
-        
-        return true
-    }
 }
 
 extension NameStreamViewController: UITextViewDelegate {
+    /// On editing start, clear the placeholder if present.
+    ///
+    /// - Parameter textView: The text view that will begain editing.
     func textViewDidBeginEditing(_ textView: UITextView) {
         if isDescriptionPlaceholder {
             textView.text = nil
@@ -131,6 +144,9 @@ extension NameStreamViewController: UITextViewDelegate {
         }
     }
     
+    /// On editing end, add the placeholder if text view is empty.
+    ///
+    /// - Parameter textView: The text view that will end editing.
     func textViewDidEndEditing(_ textView: UITextView) {
         if textView.text.isEmpty {
             textView.text = descriptionPlaceholder
