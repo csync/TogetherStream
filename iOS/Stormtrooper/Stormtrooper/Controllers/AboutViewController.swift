afb04f3e75605f85ba7626d20f6a8e2780605448
Daniel Firsht <dfirsht@gmail.com>
2017-02-21 15:48:33 -0600

    marked up about VC, removed bottom white space on about screen (#436)

--- iOS/Stormtrooper/Stormtrooper/Controllers/AboutViewController.swift
+++ iOS/Stormtrooper/Stormtrooper/Controllers/AboutViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
+/// View controller for the "About" screen.
 class AboutViewController: UIViewController {
     
     @IBOutlet weak var ourSiteTextView: UITextView!
     
+    /// The string to add a hyperlink to.
     let linkedString = "visit our site."
     
     override func viewDidLoad() {
@@ -27,42 +29,52 @@ class AboutViewController: UIViewController {
         }
     }
     
+    /// Sets up the label about visiting our site.
     private func setupOurSiteLabel() {
-        // Add hyperlink to our site
+        // Add hyperlink to our site on the proper part of the label.
         guard let attributedString = ourSiteTextView.attributedText?.mutableCopy() as? NSMutableAttributedString else {return}
         let linkRange = attributedString.mutableString.range(of: linkedString)
         guard linkRange.location != NSNotFound else {
             return
         }
         attributedString.addAttribute(NSLinkAttributeName, value: "http://ibm.biz/together-stream", range: linkRange)
+        // Format the linked text
         let linkAttributes = [
             NSForegroundColorAttributeName: UIColor.stormtrooperTextBlack,
             NSUnderlineColorAttributeName: UIColor.stormtrooperTextBlack,
             NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue] as [String : Any]
         ourSiteTextView.linkTextAttributes = linkAttributes
+        
         ourSiteTextView.attributedText = attributedString
     }
     
+    /// Open the given URL in a Safari view controller.
+    ///
+    /// - Parameter url: The URL string to open.
     private func open(url: String) {
         guard let url = URL(string: url) else { return }
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
     
+    /// Open the CSync website.
     @IBAction func tappedCSync() {
         Utils.sendGoogleAnalyticsEvent(withCategory: "About", action: "SelectedCSync")
         open(url: "https://ibm.biz/together-stream-csync-logo")
     }
     
+    /// Open the Bluemix website.
     @IBAction func tappedBluemix() {
         Utils.sendGoogleAnalyticsEvent(withCategory: "About", action: "SelectedBluemix")
         open(url: "https://ibm.biz/together-stream-bluemix-logo")
     }
     
+    /// Open the MIL website,
     @IBAction func tappedMIL() {
         Utils.sendGoogleAnalyticsEvent(withCategory: "About", action: "SelectedMIL")
         open(url: "https://ibm.biz/together-stream-mil-logo")
     }
     
+    /// Open the YouTube website.
     @IBAction func tappedYoutube() {
         Utils.sendGoogleAnalyticsEvent(withCategory: "About", action: "SelectedYoutube")
         open(url: "https://ibm.biz/together-stream-youtube-logo")
@@ -70,6 +82,14 @@ class AboutViewController: UIViewController {
 }
 
 extension AboutViewController: UITextViewDelegate {
+    /// Enable interacting with the URL in the text view.
+    ///
+    /// - Parameters:
+    ///   - textView: The text view to interact with.
+    ///   - URL: The URL to interact with.
+    ///   - characterRange: The range the URL is located in.
+    ///   - interaction: The type of interaction.
+    /// - Returns: Whether the interaction should occur.
     func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
         return true
     }
