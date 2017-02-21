95e53e79478da9b32894627765f65b2d95caf630
Daniel Firsht <dfirsht@gmail.com>
2017-02-21 14:59:00 -0600

    added markup for profile vc (#434)

--- iOS/Stormtrooper/Stormtrooper/Controllers/ProfileViewController.swift
+++ iOS/Stormtrooper/Stormtrooper/Controllers/ProfileViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 import FBSDKLoginKit
 
-class ProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+/// View controller for the "Profile" screen.
+class ProfileViewController: UIViewController {
     
     // MARK: - Outlets
     
@@ -20,47 +21,55 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     
     // MARK: - Properties
     
+    /// Shorthand for shared FacebookDataManager.
     private let facebookDataManager = FacebookDataManager.sharedInstance
     
     // MARK: - Table Rows
     
-    private lazy var rows: [ProfileRow] = {
+    /// The rows that should be displayed on the profile screen.
+    fileprivate lazy var rows: [ProfileRow] = {
+        /// The row linking to the "Invite" screen.
         let invite = ProfileRow(
             label: "Invite Friends to Together Stream",
             showDisclosure: true,
-            action: { self.pushViewController("inviteStream", from: "InviteStream")
-            {viewController in
-                guard let viewController = viewController as? InviteStreamViewController else {
-                    return
+            action: {[unowned self] in
+                Utils.sendGoogleAnalyticsEvent(withCategory: "Profile", action: "SelectedInvite")
+                self.pushViewController("inviteStream", from: "InviteStream") {viewController in
+                    guard let viewController = viewController as? InviteStreamViewController else {
+                        return
+                    }
+                    viewController.isCreatingStream = false
+                    viewController.canInviteToStream = false
+                    viewController.navigationItem.title = "Invite to App"
                 }
-                viewController.isCreatingStream = false
-                viewController.canInviteToStream = false
-                viewController.navigationItem.title = "Invite to App"
             }
-        }
         )
+        /// The row linking to the "About" screen.
         let about = ProfileRow(
             label: "About Together Stream",
             showDisclosure: true,
-            action: {
+            action: {[unowned self] in
                 Utils.sendGoogleAnalyticsEvent(withCategory: "Profile", action: "SelectedAbout")
                 self.pushViewController("about")
             }
         )
+        /// The row linking to an external feedback page.
         let feedback = ProfileRow(
             label: "Feedback",
             showDisclosure: false,
-            action: {
+            action: {[unowned self] in
                 Utils.sendGoogleAnalyticsEvent(withCategory: "Profile", action: "SelectedFeedback")
                 self.open(url: "https://ibm.biz/together-stream-feedback")
             }
         )
+        /// The row which signs out the current user and links back to the "Home" screen.
         let signOut = ProfileRow(
             label: "Sign Out of Facebook",
             showDisclosure: false,
-            action: {
+            action: {[unowned self] in
                 Utils.sendGoogleAnalyticsEvent(withCategory: "Profile", action: "SelectedSignOut")
                 self.facebookDataManager.logOut()
+                // Unauthenticates from CSync
                 CSyncDataManager.sharedInstance.unauthenticate {error in
                     DispatchQueue.main.async {
                         guard error == nil else {
@@ -101,12 +110,12 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     
     // MARK: - Helper Functions
     
-    /// Set the navigation items for this view controller
+    /// Set the navigation items for this view controller.
     private func setupNavigationItems() {
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
     
-    /// Set the profile image view using the current user's Facebook profile picture
+    /// Set the profile image view using the current user's Facebook profile picture.
     private func setupProfilePicture() {
         let facebookDataManager = FacebookDataManager.sharedInstance
         facebookDataManager.fetchProfilePictureForCurrentUser() { error, image in
@@ -118,12 +127,12 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
         }
     }
     
-    /// Set the name label with the current user's Facebook profile name
+    /// Set the name label with the current user's Facebook profile name.
     private func setupNameLabel() {
         nameLabel.text = facebookDataManager.profile?.name ?? ""
     }
     
-    /// Set the version number label
+    /// Set the version number label.
     private func setupVersionNumber() {
         let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") ?? "0.0.0"
         let build = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) ?? "0"
@@ -131,7 +140,7 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
         versionLabel.text = label
     }
     
-    /// Register the nib containing the cell with the table view
+    /// Register the nib containing the cell with the table view.
     private func setupTableView() {
         let nib = UINib(nibName: "ProfileTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "profileCell")
@@ -148,7 +157,12 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
         tableView.tableFooterView = UIView()
     }
     
-    /// Push the view controller with the given identifier onto the stack
+    /// Push the view controller with the given identifier onto the stack.
+    ///
+    /// - Parameters:
+    ///   - identifier: The identifier of the view controller.
+    ///   - storyboard: The identifier of the storyboard.
+    ///   - configure: Optional callback to configure the view controller.
     private func pushViewController(_ identifier: String, from storyboard: String = "Profile", withConfiguration configure: ((UIViewController) -> Void)? = nil) {
         let storyboard = UIStoryboard(name: storyboard, bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: identifier)
@@ -157,21 +171,34 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     }
     
     /// Open the given URL in a Safari view controller
+    ///
+    /// - Parameter url: The URL string to open.
     private func open(url: String) {
         guard let url = URL(string: url) else { return }
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
-    
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+
     // MARK: - UITableViewDataSource
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
+    /// Returns the number of rows for the section.
+    ///
+    /// - Parameters:
+    ///   - tableView: The tableview requesting the number.
+    ///   - section: The section the request is for.
+    /// - Returns: The number of rows for the section.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return rows.count
     }
     
+    /// Dequeues and configures the cell for the given path.
+    ///
+    /// - Parameters:
+    ///   - tableView: The table requesting the cell.
+    ///   - indexPath: The path of the cell.
+    /// - Returns: The cell to display.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell")
         guard let profileCell = cell as? ProfileTableViewCell else { return UITableViewCell() }
@@ -183,6 +210,11 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     
     // MARK: - UITableViewDelegate
     
+    /// Performs the action of the selected cell.
+    ///
+    /// - Parameters:
+    ///   - tableView: The table that was selected.
+    ///   - indexPath: The path of the cell that was selected.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let row = rows[indexPath.row]
@@ -190,9 +222,12 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     }
 }
 
-/// Data associated with a profile row
+/// Data associated with a profile row.
 private struct ProfileRow {
+    /// The display label of the cell.
     let label: String
+    /// Whether to show the disclosure indicator.
     let showDisclosure: Bool
+    /// The action to perform on cell selection.
     let action: (Void) -> Void
 }
