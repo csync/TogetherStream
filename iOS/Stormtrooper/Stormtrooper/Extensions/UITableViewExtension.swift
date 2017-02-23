970b20faa5d88895924a20655269826f22bfb098
Daniel Firsht <dfirsht@gmail.com>
2017-02-23 15:37:35 -0600

    marked up extensions and removed unneeded code (#449)

--- iOS/Stormtrooper/Stormtrooper/Extensions/UITableViewExtension.swift
+++ iOS/Stormtrooper/Stormtrooper/Extensions/UITableViewExtension.swift
@@ -20,13 +20,16 @@ import UIKit
 
 extension UITableView {
     
+    /// Scrolls the table view to the bottom row.
+    ///
+    /// - Parameter animated: Whether the scroll is animated or not.
     func scrollTableViewToBottom(animated: Bool) {
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
             let numberOfSections = self.numberOfSections
             let numberOfRows = self.numberOfRows(inSection: numberOfSections-1)
             if numberOfRows > 0 {
                 let indexPath = IndexPath(row: numberOfRows-1, section: (numberOfSections-1))
-                self.scrollToRow(at: indexPath, at: UITableViewScrollPosition.bottom, animated: animated)
+                self.scrollToRow(at: indexPath, at: .bottom, animated: animated)
             }
         }
     }
