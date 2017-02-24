acc5b2346c2c36e9eb118b7ece53b3a8eb8aec23
Daniel Firsht <dfirsht@gmail.com>
2017-02-24 12:57:09 -0600

    silenced unused var and objc compatibility warnings (#453)

--- iOS/Stormtrooper/Stormtrooper/Extensions/UIColor.swift
+++ iOS/Stormtrooper/Stormtrooper/Extensions/UIColor.swift
@@ -11,27 +11,27 @@ import UIKit
 
 extension UIColor {
     /// The orangle color used throughout the app.
-    class var stormtrooperOrange: UIColor {
+    @nonobjc class var stormtrooperOrange: UIColor {
         return UIColor(colorLiteralRed: 216.0 / 255, green: 65.0 / 255, blue: 6.0 / 255, alpha: 1)
     }
     
     /// The color of the dropshadow of the cards.
-    class var stormtrooperShadow: UIColor {
+    @nonobjc class var stormtrooperShadow: UIColor {
         return UIColor(colorLiteralRed: 206.0 / 255, green: 206.0 / 255, blue: 206.0 / 255, alpha: 1)
     }
     
     /// The color of the row seperators.
-    class var stormtrooperSeperatorGray: UIColor {
+    @nonobjc class var stormtrooperSeperatorGray: UIColor {
         return UIColor(colorLiteralRed: 89.0 / 255, green: 88.0 / 255, blue: 89.0 / 255, alpha: 1)
     }
     
     /// The color of the placeholder text.
-    class var stormtrooperPlaceholderGray: UIColor {
+    @nonobjc class var stormtrooperPlaceholderGray: UIColor {
         return UIColor(colorLiteralRed: 148.0 / 255, green: 147.0 / 255, blue: 148.0 / 255, alpha: 1)
     }
     
     /// The color of the text used throughout the app.
-    class var stormtrooperTextBlack: UIColor {
+    @nonobjc class var stormtrooperTextBlack: UIColor {
         return UIColor(colorLiteralRed: 74.0 / 255, green: 74.0 / 255, blue: 74.0 / 255, alpha: 1)
     }
 }
