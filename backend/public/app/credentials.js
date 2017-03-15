8519c2d8e8389cb9086f70e117f3e64d6f91f164
Jaime Guajardo <guajardo@us.ibm.com>
2017-03-15 16:07:32 -0500

    loading webapp credentials from credentials.js

---
+++ backend/public/app/credentials.js
@@ -0,0 +1,7 @@
+window.Credentials = {
+  fbAppId: '322296781487008',
+  devFbAppId: '369713986745287',
+  youtubeApiKey: 'AIzaSyBtZHBo_puHlrTw2cgqz-q5fYlK4XBuHNA',
+  csyncIpAddress: '169.46.157.54',
+  csyncPort: '6005'
+}
