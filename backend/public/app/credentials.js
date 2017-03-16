3bfac41ef8548ff5a854fa3b8aeeea1cd0656a84
Jaime Guajardo <guajardo@us.ibm.com>
2017-03-16 16:02:20 -0500

    updating credentials to be blank for webapp

--- backend/public/app/credentials.js
+++ backend/public/app/credentials.js
@@ -1,7 +1,7 @@
 window.Credentials = {
-  fbAppId: '322296781487008',
-  devFbAppId: '369713986745287',
-  youtubeApiKey: 'AIzaSyBtZHBo_puHlrTw2cgqz-q5fYlK4XBuHNA',
-  csyncIpAddress: '169.46.157.54',
-  csyncPort: '6005'
+  fbAppId: 'YOUR_PRODUCTION_FACEBOOK_APP_ID',
+  devFbAppId: 'YOUR_DEVELOPMENT_FACEBOOK_APP_ID',
+  youtubeApiKey: 'YOUR_YOUTUBE_API_KEY',
+  csyncIpAddress: 'YOUR_CSYNC_SEVER_IP_ADDRESS',
+  csyncPort: 'YOUR_CSYNC_SEVER_PORT'
 }
