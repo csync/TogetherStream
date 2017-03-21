614b3d65d78ea282330f231e226576494e5a3ef5
Daniel Firsht <dfirsht@us.ibm.com>
2017-03-21 14:10:05 -0500

    removed unneeded code

--- stream-bot/config/appVars.js
+++ stream-bot/config/appVars.js
@@ -2,29 +2,9 @@
 //  © Copyright IBM Corporation 2017
 //  LICENSE: MIT http://ibm.biz/license-non-ios
 //
-var cfenv = require("cfenv");
 var credentials = require("./private/credentials");
-var vcapServices = null;
-if (process.env.VCAP_SERVICES) {
-    vcapServices = JSON.parse(process.env.VCAP_SERVICES);
-} else {
-  vcapServices = require('./private/VCAP_SERVICES.json');
-}
-
-var appEnv = cfenv.getAppEnv();
-if (appEnv.isLocal) {
-  console.log('Defaulting to local environment config.');
-  appEnv = cfenv.getAppEnv({
-      vcap: {
-          services: vcapServices
-      }
-  });
-  vcapServices = require('./private/VCAP_SERVICES.json');
-}
 
 var appVars = {
-    port: appEnv.port,
-    bind: appEnv.bind,
     csyncServer: "localhost",
     csyncPort: 6005,
     youtubeKey: credentials.youtube.key,
