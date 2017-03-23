c37e5889c8d7f8f4587e067247276551ca71fef1
Daniel Firsht <dfirsht@us.ibm.com>
2017-03-23 15:58:30 -0500

    Show alert that streambot videos cannot be blocked

--- iOS/TogetherStream/TogetherStream/Controllers/StreamViewController.swift
+++ iOS/TogetherStream/TogetherStream/Controllers/StreamViewController.swift
@@ -768,6 +768,12 @@ class StreamViewController: UIViewController {
         let alert = UIAlertController(title: "Flag Video", message: "Are you sure you wish to flag and remove this video from the queue?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         alert.addAction(UIAlertAction(title: "Confirm", style: .destructive) {_ in
+            guard self.viewModel.stream?.hostFacebookID != "122296268288083" else {
+                let alert = UIAlertController(title: "Cannot Flag Video", message: "Stream bot's videos cannot be flagged.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(alert, animated: true)
+                return
+            }
             self.viewModel.reportVideo()
         })
         present(alert, animated: true)
